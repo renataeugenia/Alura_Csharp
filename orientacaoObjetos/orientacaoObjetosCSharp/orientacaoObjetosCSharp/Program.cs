3f840755dac51cf0a29d7a58973f3d0829a8b490
renataeugenia <eugeniacomercial@gmail.com>
2022-07-19 17:32:37 -0300

    inico orientação a objetos com C#

---
+++ orientacaoObjetos/orientacaoObjetosCSharp/orientacaoObjetosCSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("INICIANDO O PROJETO BYTEBANK");
+
+string conta = "10123-x";
+string titular = "Renata Eugênia";
+int numero_agencia = 23;
+string nome_agencia = "Paulista";
+double saldo = 70.69;
+
+Console.WriteLine("Titular: " + titular);
+Console.WriteLine("Conta: " + conta);
+Console.WriteLine("Numero_Agencia: " + numero_agencia);
+Console.WriteLine("Nome_Agencia: " + nome_agencia);
+Console.WriteLine("Saldo: R$ " + saldo);
+
+Console.ReadKey();
