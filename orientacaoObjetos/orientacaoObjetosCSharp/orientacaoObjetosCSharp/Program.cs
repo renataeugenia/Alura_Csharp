c41020ed77a4226fcecfc10a6b42c8703113c90e
renataeugenia <eugeniacomercial@gmail.com>
2022-07-26 14:43:39 -0300

    aula 26/07/22

--- orientacaoObjetos/orientacaoObjetosCSharp/orientacaoObjetosCSharp/Program.cs
+++ orientacaoObjetos/orientacaoObjetosCSharp/orientacaoObjetosCSharp/Program.cs
@@ -10,7 +10,7 @@ conta1.titular = "Renata Eugênia";
 conta1.conta = "25879 - X";
 conta1.numero_agencia = 27;
 conta1.nome_agencia = "Paulista";
-conta1.saldo = 257.69;
+conta1.saldo = 100.69;
 conta1.ativa = false ;
 
 ContaCorrente conta2 = new ContaCorrente();
@@ -19,7 +19,7 @@ conta2.titular = "Ana Paula Eugênia";
 conta2.conta = "36957 - F";
 conta2.numero_agencia = 40;
 conta2.nome_agencia = "Higienópolis";
-conta2.saldo = 3900.50;
+conta2.saldo = 200.87;
 conta2.ativa = true;
 
 ContaCorrente conta3 = new ContaCorrente();
@@ -49,5 +49,20 @@ Console.WriteLine("Numero_Agencia: " + conta3.numero_agencia);
 Console.WriteLine("Nome_Agencia: " + conta3.nome_agencia);
 Console.WriteLine($"Saldo: R$ {String.Format("{0:0.00}", conta3.saldo)}");
 Console.WriteLine($"conta ativa: {conta3.ativa}");
+
+Console.WriteLine($"Saldo da Ana Paula pré-saque : R$ {String.Format("{0:0.00}", conta2.saldo)}");
+conta2.Sacar(50);
+Console.WriteLine($"Saldo da Ana Paula pós-saque : R$ {String.Format("{0:0.00}", conta2.saldo)}");
+conta2.Depositar(60);
+Console.WriteLine($"Saldo da Ana Paula pós-deposito : R$ {String.Format("{0:0.00}", conta2.saldo)}");
+
+Console.WriteLine($"Saldo da Ana Paula pré-transferencia : R$ {String.Format("{0:0.00}", conta2.saldo)}");
+Console.WriteLine($"Saldo da Renata pré-transferencia : R$ {String.Format("{0:0.00}", conta1.saldo)}");
+
+bool transferencia = conta1.Transferir(50, conta2);
+Console.WriteLine("transferencia realizada com sucesso? : " +transferencia);
+
+Console.WriteLine($"Saldo da Ana Paula pós-transferencia : R$ {String.Format("{0:0.00}", conta2.saldo)}");
+Console.WriteLine($"Saldo da Renata pós-transferencia : R$ {String.Format("{0:0.00}", conta1.saldo)}");
 
 Console.ReadKey();
--- orientacaoObjetos/orientacaoObjetosCSharp/orientacaoObjetosCSharp/contaCorrente.cs
+++ orientacaoObjetos/orientacaoObjetosCSharp/orientacaoObjetosCSharp/contaCorrente.cs
@@ -8,6 +8,38 @@
         public string nome_agencia;
         public double saldo;
         public bool ativa;
+
+        public bool Sacar(double valor)
+        {
+            if(saldo < valor || valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            saldo = saldo + valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if(saldo < valor || valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                destino.saldo = destino.saldo + valor;
+                return true;
+            }
+        }
             
     }
 }
